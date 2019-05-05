7dc1fc2d43cfbd9dc673a8b97bdd37e611db98e3
zifans <903953451@qq.com>
2019-05-05 17:27:42 +0800

    提交多个文件

--- js/nodestudy.js
+++ js/nodestudy.js
@@ -1,61 +1,14 @@
-/*console.log("hello world zgj")
-
-var http=require("http");
-
-http.createServer(function(request,response){
-    response.writeHead(200,{'Content-Type':'text/plain'});
-    response.end('hello world\n');
-}).listen(8888);
-
-console.log('Server running at http://127.0.0.1:8888/');
-
-var x=30;
-var y=210;
-var sum=x+y;
-console.log(sum);
-
-阻塞代码实例
-var fs=require("fs");
-var data=fs.readFileSync('input.txt');
-console.log(data.toString());
-console.log("程序执行结束");
-
-非阻塞代码实例
-var fs=require("fs");
-fs.readFile('input.txt',function(err,data){
-    if(err)
-        return console.error(err);
-    console.log(data.toString());
-});
-console.log("程序执行结束");
-
-var events=require('events');
-var eventEmitter=new events.EventEmitter();
-
-var connectHandler=function connected(){
-    console.log("连接成功");
-    eventEmitter.emit('data_received');
-}
-
-eventEmitter.on('connection',connectHandler);
-
-eventEmitter.on('data_received',function(){
-    console.log("数据库连接成功");
-});
-
-eventEmitter.emit('connection');
-console.log("程序执行完毕");
-*/
-
-var fs=require("fs");
-fs.readFile("input.txt",function(err,data){
-    if(err)
-    {
-        console.log(err.stack);
-        return;
-    }
-    console.log(data.toString());
-});
-console.log('程序执行完毕');
-
-   
+//express_demo.js 文件
+var express = require('C:/Users/Administrator/AppData/Roaming/npm/node_modules/express');
+var app = express();
+ 
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+ 
+var server = app.listen(8081, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+})
